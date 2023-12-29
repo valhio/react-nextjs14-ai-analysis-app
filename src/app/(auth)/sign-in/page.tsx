8ec6bb2f88660810a72e53f60ad20ed36de09611
valhio <valentingerasimow@gmail.com>
2023-12-29 03:15:50 +0200

    added correct info about the app

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -22,11 +22,10 @@ export default function Page() {
         {/* App Title */}
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold tracking-tight text-gray-900">
-            RilaTalks
+            DocAI
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
-            RilaTalks is a simple chat application that allows you to chat with
-            your friends.
+            DocAI is a document interpretation tool that uses AI to extract data from documents.
           </p>
         </div>
 
