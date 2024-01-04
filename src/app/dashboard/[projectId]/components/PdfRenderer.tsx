9d8d29adea58711cf64289e04e6283a194d786ee
valhio <valentingerasimow@gmail.com>
2024-01-04 14:55:03 +0200

    added rendering of pdf files

--- src/app/dashboard/[projectId]/components/PdfRenderer.tsx
+++ src/app/dashboard/[projectId]/components/PdfRenderer.tsx
@@ -1,7 +1,26 @@
-const PdfRenderer = () => {
+"use client";
+
+import { redirect } from "next/navigation";
+import { Document, Page, pdfjs } from "react-pdf";
+
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+interface PdfRendererProps {
+  url: string
+}
+
+const PdfRenderer = ({
+  url
+}: PdfRendererProps) => {
   return (
     <div>
       <h1>PDF Renderer</h1>
+      <Document file={url}>
+        <Page pageNumber={1} />
+      </Document>
     </div>
   );
 };
--- src/app/dashboard/[projectId]/page.tsx
+++ src/app/dashboard/[projectId]/page.tsx
@@ -1,9 +1,11 @@
 import getFileById from "@/actions/getFileById";
 import getSession from "@/actions/getSession";
-import { File } from "@prisma/client";
-import { notFound } from "next/navigation";
+import { File, User } from "@prisma/client";
+import { notFound, redirect } from "next/navigation";
 import PdfRenderer from "./components/PdfRenderer";
 import ChatWrapper from "./components/ChatWrapper";
+import { getFileFromS3, getFileUrlFromS3 } from "@/utils/s3";
+import getCurrentUser from "@/actions/getCurrentUser";
 
 interface PageProps {
   params: {
@@ -14,9 +16,17 @@ interface PageProps {
 const Page = async ({ params }: PageProps) => {
   const { projectId } = params;
 
-  const file = await getFileById(projectId);
+  const user = await getCurrentUser();
+  if (!user || !user.id || !user.email) redirect("/");
 
+  const file = await getFileById(projectId);
   if (!file) notFound();
+  
+  const fetchedFile = await getFileFromS3(file.name, user.id);
+  if (!fetchedFile) notFound();
+  
+  const fileUrl = await getFileUrlFromS3(file.name, user.id);
+  if (!fileUrl) notFound();
 
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
@@ -24,7 +34,7 @@ const Page = async ({ params }: PageProps) => {
         {/* Left Side */}
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer />
+            <PdfRenderer url={fileUrl}/>
           </div>
         </div>
 
